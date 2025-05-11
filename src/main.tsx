10b646d01f4f6846fa666cc66518df220774f5a5
mukhamux <mukhamux@mail.ru>
2025-05-11 21:45:20 +0000

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import {TonConnectUIProvider} from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://mukhamux.github.io/cstoken_test/tonconnect-manifest.json';
+const manifestUrl = 'https://mukhamux.github.io/cstoken_test/public/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <TonConnectUIProvider manifestUrl={manifestUrl}>
